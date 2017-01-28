532a43a94140e3bf38e195151d9dcd8d13f0d3b7
Kevin Robinson <kevinr.zan@gmail.com>
2017-01-28 12:52:11 -0800

    js work

--- game.js
+++ game.js
@@ -8,11 +8,12 @@ var answers = [
   ['crow', 'crows'],
   ['fruit', 'nuts', 'peanuts', 'eggs', 'bread', 'grain']
 ];
+var correctAnswers = 0;
 
 var username = prompt('What\'s your name?');
 alert('Hello, ' + username + '. Thanks for coming to my site!');
 console.log('User name: ' + username);
-var correct = 0;
+
 /*
 we want to ask the user a question
 take answer as input
@@ -25,7 +26,7 @@ depending on T/F, provide feedback
 var answer0 = prompt('say, ' + username + ', was I born around Seattle?');
 if(logAnswer(answer0, answers[0], 0) === true) {
   alert('Correct! I\'m a Seattlite.');
-  correct++;
+  correctAnswers++;
 }
 else {
   alert('Too bad! Try again.');
@@ -34,7 +35,7 @@ else {
 var answer1 = prompt('say, ' + username + ', did I have dogs growing up?');
 if(logAnswer(answer1, answers[1], 1) === true) {
   alert('Correct! I had cats growing up, not dogs.');
-  correct++;
+  correctAnswers++;
 }
 else {
   alert('Too bad! Try again.');
@@ -43,7 +44,7 @@ else {
 var answer2 = prompt('say, ' + username + ', have I done any coding before?');
 if(logAnswer(answer2, answers[2], 2)) {
   alert('Correct! I\'m actually working on a project of my own right now.');
-  correct++;
+  correctAnswers++;
 }
 else {
   alert('Too bad! Try again.');
@@ -52,7 +53,7 @@ else {
 var answer3 = prompt('say, ' + username + ', do I speak any foreign languages?');
 if(logAnswer(answer3, answers[3], 3)) {
   alert('Correct! I want to learn Japanese though.');
-  correct++;
+  correctAnswers++;
 }
 else {
   alert('Too bad! Try again.');
@@ -61,7 +62,7 @@ else {
 var answer4 = prompt('say, ' + username + ', what kind of wild animal do I feed?');
 if(checkAnswerMulti(answer4, answers[4], 4) === true) {
   alert('Correct! I want to make friends with them!');
-  correct++;
+  correctAnswers++;
 }
 else {
   alert('Too bad! Try again.');
@@ -70,20 +71,20 @@ else {
 var answer5 = prompt('say, ' + username + ', what are some good things to feed crows?');
 if(checkAnswerMulti(answer5, answers[5], 5) === true) {
   alert('Correct! I usually feed mine peanuts.');
-  correct++;
+  correctAnswers++;
 }
 else {
   alert('Too bad! Try again.');
 }
 
-console.log('Correct answers: ' + correct + '/' + answers.length);
-if(correct === answers.length)
+console.log('Correct answers: ' + correctAnswers + '/' + answers.length);
+if(correctAnswers === answers.length)
 {
   alert('Congrats! You got all the answers correct!');
 }
 else
 {
-  alert('You got ' + correct + ' correct out of ' + answers.length + ' questions');
+  alert('You got ' + correctAnswers + ' correct out of ' + answers.length + ' questions');
 }
 
 //wrapper for checkAnswer that prints the result to the console
@@ -114,6 +115,13 @@ function checkAnswer(ans, real) {
   return ans === real;
 }
 
+/**
+ * answer checker for multiple answers. Checks given answer against each possible answer
+ * @param  {[type]} ans     [description]
+ * @param  array    reals   array of possible correct answers
+ * @param  number   number  the number of the current question. used in logging
+ * @return bool             whether the answer was correct
+ */
 function checkAnswerMulti(ans, reals, number) {
   for(var i = 0; i < reals.length; i++) {
     if(checkAnswer(ans, reals[i])) {
